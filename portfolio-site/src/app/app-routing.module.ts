61b7372dd26ac7a447606be78c48491589f5c3c2
Brendan Rae <brae23hockey@gmail.com>
2022-11-15 00:42:52 -0500

    Fixed palette and dark theme, added about and contact, and added routing in nav component

--- portfolio-site/src/app/app-routing.module.ts
+++ portfolio-site/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from 'src/libs/admin/src/lib/components/admin.component';
+import { AboutComponent } from 'src/libs/home/src/lib/about/about.component';
+import { ContactComponent } from 'src/libs/home/src/lib/contact/contact.component';
 import { HomeComponent } from 'src/libs/home/src/lib/home/home.component';
 
 const routes: Routes = [
--- portfolio-site/src/libs/home/src/lib/home.module.ts
+++ portfolio-site/src/libs/home/src/lib/home.module.ts
@@ -3,17 +3,30 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+
+export const routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+]
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NavComponent
+    NavComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     MatToolbarModule,
     MatButtonModule
   ],
   exports: [
+    RouterModule,
     HomeComponent,
     NavComponent
   ]
